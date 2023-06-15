c09e3618fba0904fb639cbc63dee5e95f09d384c
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-06-15 12:37:06 +0800

    [Feat][Lex] Created User model and prisma service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { RequestLoggerMiddleware } from "@common/middleware";
-import { MiddlewareConsumer, Module } from "@nestjs/common";
+import { Logger, MiddlewareConsumer, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { PrismaModule, loggingMiddleware } from "nestjs-prisma";
 
 import config from "@config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
@@ -12,6 +14,19 @@ import { AppService } from "./app.service";
       isGlobal: true,
       load: [config],
     }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          loggingMiddleware({
+            logger: new Logger("PrismaMiddleware"),
+            logLevel: "log",
+          }),
+        ],
+      },
+    }),
+
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
